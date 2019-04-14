6352414bc29aea3bb6fd9bb1cac2a1a5e0fe014a
cesarbahena <cabahenac@gmail.com>
2019-04-14 13:17:28 -0600

    refactor(controllers): enhance shift handovers with DTOs and relations

--- Controllers/ShiftHandoversController.cs
+++ Controllers/ShiftHandoversController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuimiosHub.Data;
+using QuimiosHub.DTOs;
 using QuimiosHub.Models;
 
 namespace QuimiosHub.Controllers;
@@ -17,7 +18,7 @@ public class ShiftHandoversController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<ShiftHandover>>> GetShiftHandovers(
+    public async Task<ActionResult<IEnumerable<ShiftHandoverDto>>> GetShiftHandovers(
         [FromQuery] DateTime? startDate = null,
         [FromQuery] DateTime? endDate = null)
     {
@@ -36,33 +37,99 @@ public class ShiftHandoversController : ControllerBase
 
         var handovers = await query
             .OrderByDescending(sh => sh.HandoverDate)
+            .Select(sh => new ShiftHandoverDto
+            {
+                Id = sh.Id,
+                ShiftId = sh.ShiftId,
+                ShiftName = sh.Shift.Name,
+                UserId = sh.UserId,
+                UserName = sh.User.FullName,
+                HandoverDate = sh.HandoverDate,
+                Notes = sh.Notes,
+                PendingSamplesCount = sh.PendingSamplesCount,
+                PendingSamples = sh.PendingSamples.Select(ps => new PendingSampleDto
+                {
+                    SampleId = ps.SampleId,
+                    FolioGrd = ps.Sample.FolioGrd,
+                    Reason = ps.Reason
+                }).ToList()
+            })
             .ToListAsync();
 
         return Ok(handovers);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<ShiftHandover>> GetShiftHandover(int id)
+    public async Task<ActionResult<ShiftHandoverDto>> GetShiftHandover(int id)
     {
         var handover = await _context.ShiftHandovers
             .Include(sh => sh.Shift)
             .Include(sh => sh.User)
             .Include(sh => sh.PendingSamples)
                 .ThenInclude(ps => ps.Sample)
-            .FirstOrDefaultAsync(sh => sh.Id == id);
+            .Where(sh => sh.Id == id)
+            .Select(sh => new ShiftHandoverDto
+            {
+                Id = sh.Id,
+                ShiftId = sh.ShiftId,
+                ShiftName = sh.Shift.Name,
+                UserId = sh.UserId,
+                UserName = sh.User.FullName,
+                HandoverDate = sh.HandoverDate,
+                Notes = sh.Notes,
+                PendingSamplesCount = sh.PendingSamplesCount,
+                PendingSamples = sh.PendingSamples.Select(ps => new PendingSampleDto
+                {
+                    SampleId = ps.SampleId,
+                    FolioGrd = ps.Sample.FolioGrd,
+                    Reason = ps.Reason
+                }).ToList()
+            })
+            .FirstOrDefaultAsync();
 
         if (handover == null)
-            return NotFound();
+            return NotFound(new { message = "Shift handover not found" });
 
         return Ok(handover);
     }
 
     [HttpPost]
-    public async Task<ActionResult<ShiftHandover>> CreateShiftHandover(ShiftHandover handover)
+    public async Task<ActionResult<ShiftHandoverDto>> CreateShiftHandover(CreateShiftHandoverDto dto)
     {
+        if (!await _context.Shifts.AnyAsync(s => s.Id == dto.ShiftId))
+            return BadRequest(new { message = "Invalid shift ID" });
+
+        if (!await _context.Users.AnyAsync(u => u.Id == dto.UserId))
+            return BadRequest(new { message = "Invalid user ID" });
+
+        var handover = new ShiftHandover
+        {
+            ShiftId = dto.ShiftId,
+            UserId = dto.UserId,
+            HandoverDate = dto.HandoverDate,
+            Notes = dto.Notes,
+            PendingSamplesCount = dto.PendingSamples.Count,
+            CreatedAt = DateTime.UtcNow
+        };
+
         _context.ShiftHandovers.Add(handover);
         await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetShiftHandover), new { id = handover.Id }, handover);
+        foreach (var ps in dto.PendingSamples)
+        {
+            var pendingSample = new PendingSample
+            {
+                ShiftHandoverId = handover.Id,
+                SampleId = ps.SampleId,
+                Reason = ps.Reason,
+                CreatedAt = DateTime.UtcNow
+            };
+            _context.PendingSamples.Add(pendingSample);
+        }
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetShiftHandover), new { id = handover.Id },
+            await GetShiftHandover(handover.Id));
     }
 }
