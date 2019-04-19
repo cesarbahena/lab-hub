a6fb17d1cd10b61cdd811f7dd44091ad56652410
cesarbahena <cabahenac@gmail.com>
2019-04-19 10:22:47 -0600

    feat(routes): add DTOs for collection routes with nested stops

--- Controllers/CollectionRoutesController.cs
+++ Controllers/CollectionRoutesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuimiosHub.Data;
+using QuimiosHub.DTOs;
 using QuimiosHub.Models;
 
 namespace QuimiosHub.Controllers;
@@ -17,50 +18,137 @@ public class CollectionRoutesController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<CollectionRoute>>> GetCollectionRoutes(
+    public async Task<ActionResult<IEnumerable<CollectionRouteDto>>> GetCollectionRoutes(
         [FromQuery] bool? isActive = null)
     {
         var query = _context.CollectionRoutes
-            .Include(cr => cr.RouteStops.OrderBy(rs => rs.SequenceOrder))
+            .Include(cr => cr.RouteStops)
             .AsQueryable();
 
         if (isActive.HasValue)
             query = query.Where(cr => cr.IsActive == isActive.Value);
 
-        var routes = await query.OrderBy(cr => cr.Name).ToListAsync();
+        var routes = await query
+            .OrderBy(cr => cr.Name)
+            .Select(cr => new CollectionRouteDto
+            {
+                Id = cr.Id,
+                Name = cr.Name,
+                Description = cr.Description,
+                IsActive = cr.IsActive,
+                TotalStops = cr.RouteStops.Count,
+                ActiveStops = cr.RouteStops.Count(rs => rs.IsActive),
+                RouteStops = cr.RouteStops
+                    .OrderBy(rs => rs.SequenceOrder)
+                    .Select(rs => new RouteStopDto
+                    {
+                        Id = rs.Id,
+                        SequenceOrder = rs.SequenceOrder,
+                        LocationName = rs.LocationName,
+                        Address = rs.Address,
+                        Latitude = rs.Latitude,
+                        Longitude = rs.Longitude,
+                        ContactName = rs.ContactName,
+                        ContactPhone = rs.ContactPhone,
+                        IsActive = rs.IsActive
+                    }).ToList()
+            })
+            .ToListAsync();
 
         return Ok(routes);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<CollectionRoute>> GetCollectionRoute(int id)
+    public async Task<ActionResult<CollectionRouteDto>> GetCollectionRoute(int id)
     {
         var route = await _context.CollectionRoutes
-            .Include(cr => cr.RouteStops.OrderBy(rs => rs.SequenceOrder))
-            .FirstOrDefaultAsync(cr => cr.Id == id);
+            .Include(cr => cr.RouteStops)
+            .Where(cr => cr.Id == id)
+            .Select(cr => new CollectionRouteDto
+            {
+                Id = cr.Id,
+                Name = cr.Name,
+                Description = cr.Description,
+                IsActive = cr.IsActive,
+                TotalStops = cr.RouteStops.Count,
+                ActiveStops = cr.RouteStops.Count(rs => rs.IsActive),
+                RouteStops = cr.RouteStops
+                    .OrderBy(rs => rs.SequenceOrder)
+                    .Select(rs => new RouteStopDto
+                    {
+                        Id = rs.Id,
+                        SequenceOrder = rs.SequenceOrder,
+                        LocationName = rs.LocationName,
+                        Address = rs.Address,
+                        Latitude = rs.Latitude,
+                        Longitude = rs.Longitude,
+                        ContactName = rs.ContactName,
+                        ContactPhone = rs.ContactPhone,
+                        IsActive = rs.IsActive
+                    }).ToList()
+            })
+            .FirstOrDefaultAsync();
 
         if (route == null)
-            return NotFound();
+            return NotFound(new { message = "Collection route not found" });
 
         return Ok(route);
     }
 
     [HttpPost]
-    public async Task<ActionResult<CollectionRoute>> CreateCollectionRoute(CollectionRoute route)
+    public async Task<ActionResult<CollectionRouteDto>> CreateCollectionRoute(CreateCollectionRouteDto dto)
     {
+        var route = new CollectionRoute
+        {
+            Name = dto.Name,
+            Description = dto.Description,
+            IsActive = true,
+            CreatedAt = DateTime.UtcNow
+        };
+
         _context.CollectionRoutes.Add(route);
         await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetCollectionRoute), new { id = route.Id }, route);
+        foreach (var stopDto in dto.RouteStops)
+        {
+            var stop = new RouteStop
+            {
+                CollectionRouteId = route.Id,
+                SequenceOrder = stopDto.SequenceOrder,
+                LocationName = stopDto.LocationName,
+                Address = stopDto.Address,
+                Latitude = stopDto.Latitude,
+                Longitude = stopDto.Longitude,
+                ContactName = stopDto.ContactName,
+                ContactPhone = stopDto.ContactPhone,
+                Notes = stopDto.Notes,
+                IsActive = true,
+                CreatedAt = DateTime.UtcNow
+            };
+            _context.RouteStops.Add(stop);
+        }
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetCollectionRoute), new { id = route.Id },
+            await GetCollectionRoute(route.Id));
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCollectionRoute(int id, CollectionRoute route)
+    public async Task<IActionResult> UpdateCollectionRoute(int id, UpdateCollectionRouteDto dto)
     {
-        if (id != route.Id)
-            return BadRequest();
+        var route = await _context.CollectionRoutes.FindAsync(id);
+        if (route == null)
+            return NotFound(new { message = "Collection route not found" });
 
-        _context.Entry(route).State = EntityState.Modified;
+        if (dto.Name != null)
+            route.Name = dto.Name;
+
+        if (dto.Description != null)
+            route.Description = dto.Description;
+
+        if (dto.IsActive.HasValue)
+            route.IsActive = dto.IsActive.Value;
 
         try
         {
---
+++ DTOs/CollectionRouteDto.cs
@@ -0,0 +1,75 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace QuimiosHub.DTOs;
+
+public class CollectionRouteDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+    public int TotalStops { get; set; }
+    public int ActiveStops { get; set; }
+    public List<RouteStopDto> RouteStops { get; set; } = new();
+}
+
+public class RouteStopDto
+{
+    public int Id { get; set; }
+    public int SequenceOrder { get; set; }
+    public string LocationName { get; set; } = string.Empty;
+    public string? Address { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    public string? ContactName { get; set; }
+    public string? ContactPhone { get; set; }
+    public bool IsActive { get; set; }
+}
+
+public class CreateCollectionRouteDto
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(1000)]
+    public string? Description { get; set; }
+
+    public List<CreateRouteStopDto> RouteStops { get; set; } = new();
+}
+
+public class CreateRouteStopDto
+{
+    [Required]
+    public int SequenceOrder { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string LocationName { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Address { get; set; }
+
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+
+    [MaxLength(200)]
+    public string? ContactName { get; set; }
+
+    [MaxLength(50)]
+    public string? ContactPhone { get; set; }
+
+    [MaxLength(1000)]
+    public string? Notes { get; set; }
+}
+
+public class UpdateCollectionRouteDto
+{
+    [MaxLength(200)]
+    public string? Name { get; set; }
+
+    [MaxLength(1000)]
+    public string? Description { get; set; }
+
+    public bool? IsActive { get; set; }
+}
